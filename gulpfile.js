acd98e845c98198ec2087fd0c8f7963756194a94
Ben Williams <benj@minwilliams.net>
2016-02-16 21:13:47 +1100

    major rewrite of gulpfile. Breaking changes to output

--- gulpfile.js
+++ gulpfile.js
@@ -1,133 +1,80 @@
-// Load plugins
-var gulp          = require('gulp'),
-    sass          = require('gulp-ruby-sass'),
-    autoprefixer  = require('gulp-autoprefixer'),
-    jshint        = require('gulp-jshint'),
-    concat        = require('gulp-concat'),
-    uglify        = require('gulp-uglify'),
-    rename        = require('gulp-rename'),
-    cache         = require('gulp-cache'),
-    scsslint      = require('gulp-scss-lint'),
-    cmq           = require('gulp-combine-media-queries'),
-    gulpFilter    = require('gulp-filter')
+"use strict";
 
-// Default Tasks
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var autoprefixer = require('gulp-autoprefixer');
+var del = require('del');
+var cssnano = require('gulp-cssnano');
 
-var paths       = {
-                    src: {
-                      css : [
-                        'src/styles/vendor/*.css',
-                        'src/styles/css/*.css',
-                      ],
-                      sass: 'src/styles/style.scss',
-                      js: [
-                        'src/js/vendor/**/*.js', // vendor first
-                        'src/js/scripts/**/*.js',
-                        'src/js/scripts.js'
-                      ],
-                      image: 'src/images/**/*'
-                    },
-                    tests: {
-                      src: 'src/js/test/*.js',
-                      dest: 'src/js/test/build'
-                    },
-                    dest: {
-                      sass: 'src/styles/css',
-                      css: 'build/css',
-                      js: 'build/js',
-                      image: 'build/images'
-                    }
-                  };
+var sassDir = {
+  "src": 'src/sass/**/*.scss',
+  "dest": 'build/'
+};
+var cssDir = {
+  "src": 'src/css/**/*.css',
+  "dest": 'build/'
+};
 
-gulp.task('test', function(){
-  return gulp.src(["src/test/spec.js"])
-    .pipe(protractor({
-      configFile: "src/test/conf.js",
-      args: ['--baseUrl', 'http://127.0.0.1:8000']
-    }))
-    .on('error', function(e) { throw e })
+var js = {
+  "src": 'src/js/**/*.js',
+  "dest": 'build/'
+};
+
+gulp.task('clean', function(){
+  return del([
+    'build/'
+  ]);
 });
 
-// SASS
-gulp.task('sass', function() {
-  return gulp.src(paths.src.sass)
-    .pipe(sass({ style: 'expanded', lineNumbers: true }))
-    .pipe(autoprefixer('last 2 version', 'ie 9', 'ios 6', 'android 4'))
-    .pipe(cmq({log: true}))
-    .pipe(gulp.dest(paths.dest.sass));
-});
-
-gulp.task('sass-prod', function() {
-  return gulp.src(paths.src.sass)
-    .pipe(sass({ style: 'compressed', lineNumbers: false }))
-    .pipe(autoprefixer('last 2 version', 'ie 9', 'ios 6', 'android 4'))
-    .pipe(cmq({log: true}))
-    .pipe(gulp.dest(paths.dest.sass));
-});
-
-//CSS
-gulp.task('css', function(){
-  return gulp.src(paths.src.css)
-        .pipe(concat('style.css'))
-        //.pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest(paths.dest.css))
-});
-
-// Scripts
 gulp.task('scripts', function() {
-  return gulp.src(paths.src.js)
+  return gulp.src(js.src)
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('default'))
     .pipe(concat('scripts.js'))
-    .pipe(gulp.dest(paths.dest.js))
+    .pipe(gulp.dest(js.dest))
 });
 
-gulp.task('scripts-prod', function() {
-  return gulp.src(paths.src.js)
+gulp.task('scripts:minify', ['clean'], function() {
+  return gulp.src(js.src)
     .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('default'))
     .pipe(concat('scripts.js'))
     .pipe(uglify())
-    .pipe(gulp.dest(paths.dest.js))
+    .pipe(gulp.dest(js.dest))
 });
 
-
-// Images
-gulp.task('images', function() {
-  return gulp.src(paths.src.image)
-    .pipe(gulp.dest(paths.dest.image))
+gulp.task('sass', ['clean'], function () {
+  return gulp.src(sassDir.src)
+    .pipe(concat('style.scss'))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer('last 2 version', 'ie 9', 'ios 6', 'android 4'))
+    .pipe(gulp.dest(sassDir.dest))
 });
 
+gulp.task('sass:prod', function () {
+  return gulp.src(sassDir.src)
+    .pipe(concat('style.scss'))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer('last 2 version', 'ie 9', 'ios 6', 'android 4'))
+    .pipe(cssnano())
+    .pipe(gulp.dest(sassDir.dest))
+});
 
-// Watch Dev
+gulp.task('vendorCss', ['clean'], function () {
+  return gulp.src(cssDir.src)
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest(sassDir.dest))
+});
+
 gulp.task('watch', function() {
-
-  // Watch .scss files
-  gulp.watch('src/styles/**/*.scss', { interval: 1500 }, ['sass']);
-
-  // Watch .css files
-  gulp.watch('src/styles/css/**/*.css', { interval: 1500 }, ['css']);
-
-  // Watch .js files
-  gulp.watch('src/js/**/*.js', { interval: 1500 }, ['scripts']);
-
-  // Watch image files
-  gulp.watch('src/images/**/*', { interval: 1500 }, ['images']);
-
-
-});
-
-// Lint
-gulp.task('lint', function() {
-  gulp.src('assets/src/sass/**/*.scss')
-    .pipe(scsslint());
+  gulp.watch(js.src, ['scripts']);
+  gulp.watch(sassDir.src, ['sass']);
+  gulp.watch(cssDir.src, ['vendorCss']);
 });
 
 
+gulp.task('default', ['clean', 'scripts:minify','vendorCss', 'sass:prod']);
+gulp.task('dev', ['clean','scripts','vendorCss','sass', 'watch']);
 
-/* TASKS
-========================================================================== */
-
-gulp.task('default', ['sass-prod', 'css', 'scripts-prod', 'images']);
-
-gulp.task('dev', ['sass', 'css', 'scripts', 'images', 'watch']);
